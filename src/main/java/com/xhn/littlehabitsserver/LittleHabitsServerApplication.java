0f7a0444a32672007556309c6aa78d12349b15ab
肖海南 <hain.work@outlook.com>
2024-10-29 16:39:32 +0800

    初始化

---
+++ src/main/java/com/xhn/littlehabitsserver/LittleHabitsServerApplication.java
@@ -0,0 +1,13 @@
+package com.xhn.littlehabitsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleHabitsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleHabitsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xhn/littlehabitsserver/LittleHabitsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhn.littlehabitsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleHabitsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
